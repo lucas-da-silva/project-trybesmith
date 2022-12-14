e2b258a70fe2db6854dc216095329201fa8007df
lucas-da-silva <lucassouza00012345@gmail.com>
2022-12-14 15:01:41 -0300

    feat: create statusCodes.ts file aand it was implemented in product.controller.ts file

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
 import productService from '../services/product.service';
+import statusCodes from '../statusCodes';
 
 const create = async (req: Request, res: Response) => {
   const newProduct = await productService.create(req.body);
-  res.status(201).json(newProduct);
+  res.status(statusCodes.CREATED).json(newProduct);
 };
 
 export default { create };
