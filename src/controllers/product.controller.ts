64aa9672ef3eeaa7ddbd654fa6c9b5a463283732
lucas-da-silva <lucassouza00012345@gmail.com>
2022-12-14 14:53:24 -0300

    feat: create function created in product.controller.ts file

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,9 @@
+import { Request, Response } from 'express';
+import productService from '../services/product.service';
+
+const create = async (req: Request, res: Response) => {
+  const newProduct = await productService.create(req.body);
+  res.status(200).json(newProduct);
+};
+
+export default { create };
--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,7 +1,7 @@
 import productModel from '../models/product.model';
 import { TProduct } from '../types/product.type';
 
-const create = async (product: TProduct) => {
+const create = async (product: TProduct): Promise<TProduct> => {
   const { name, amount } = product;
   const newProduct = await productModel.create(name, amount);
   return newProduct;
