974062928e6fa6453efcec5218e25a28c221b02e
lucas-da-silva <lucassouza00012345@gmail.com>
2022-12-14 14:59:18 -0300

    feat: finished the route POST /products

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -3,7 +3,7 @@ import productService from '../services/product.service';
 
 const create = async (req: Request, res: Response) => {
   const newProduct = await productService.create(req.body);
-  res.status(200).json(newProduct);
+  res.status(201).json(newProduct);
 };
 
 export default { create };
